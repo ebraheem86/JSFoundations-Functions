1b551c5bc716f3ee6df0559ce7469a124e0ffed7
ebraheem86 <dejetal@hotmail.com>
2021-09-21 20:25:50 +0300

    function and yar install

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`)
 }
 
 /**
@@ -21,6 +21,11 @@ function greet(name) {
  */
 function isOdd(n) {
   // Your code here
+  if (n % 2 === 0) {
+    return false
+  } else {
+    return true
+  }
 }
 
 /**
@@ -34,6 +39,7 @@ function isOdd(n) {
  */
 function oddsSmallerThan(n) {
   // Your code here
+  return parseInt(n / 2)
 }
 
 /**
@@ -48,6 +54,12 @@ function oddsSmallerThan(n) {
  */
 function squareOrDouble(n) {
   // Your code here
+  if(isOdd(n)){
+    return n * n
+  }
+  else {
+    return n * 2
+  }
 }
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
